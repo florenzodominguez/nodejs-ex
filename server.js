3e350205ebb306bfd363fe7170e6668c30a8e575
florenzodominguez <71065752+florenzodominguez@users.noreply.github.com>
2021-01-15 13:59:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,11 +11,10 @@ const logger = createLogger({
   transports: [new transports.Console()]
 });
 
-msg = 'RSAP0001I: Transaction OK'
-logger.info(msg, {"errCode": "RSAP0001I", "transactionTime": delay})
-
 msg = 'RSAP0010E: Severe problem detected'
-logger.error(msg, {"errorCode": "RSAP0010E", "transactionTime": delay})
+logger.error(msg)
+msg = 'RSAP0001I: Transaction OK'
+logger.info(msg)
 
 //  OpenShift sample Node application
 var express = require('express'),
